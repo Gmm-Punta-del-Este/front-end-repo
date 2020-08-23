5cc43022e185bbb7545f81fa6e04baf8f7fe8298
Facundo <facundolupacchino@gmail.com>
2020-08-23 06:12:53 -0300

    Sidenav andando, User andando, Navegacion completa, falta un agregado de search bar para mas adelante

--- teste/src/app/components/toolbar/toolbar.component.ts
+++ teste/src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 
+export interface SeccionApto {
+  relleno: boolean;
+  icono: string;
+  tipo: string;
+}
+export interface SeccionNotas {
+  icono: string;
+  tipo: string;
+}
 
 @Component({
   selector: 'app-toolbar',
@@ -11,7 +20,38 @@ export class ToolbarComponent implements OnInit {
 
   @ViewChild(MatSidenav) sidenav: MatSidenav;
   opened: boolean = true;
+  Apto: SeccionApto[] = [
+    {
+      relleno: true,
+      icono: 'apartment',
+      tipo: 'InHouse',
+    },
+    {
+      relleno: true,
+      icono: 'house',
+      tipo: 'Ingresos',
+    },
+    {
+      relleno: false,
+      icono: 'house',
+      tipo: 'Egresos',
+    }
+  ];
+  Notas: SeccionNotas[] = [
+    {
+      icono: 'article',
+      tipo: 'Generales'
+    },
+    {
+      icono: 'account_balance',
+      tipo: 'Apartamento'
+    },
+    {
+      icono: 'assignment',
+      tipo: 'Recepcion'
+    },
 
+  ];
   ngOnInit(){
 
   }
